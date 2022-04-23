59ddf57db6e1446f5e9e769bb37345e41a970739
Nate Koch <31897172+natekoch@users.noreply.github.com>
2022-04-22 22:24:14 -0700

    initial setup of game layout

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const duck = document.querySelector('.duck')
+    const gameDisplay = document.querySelector('.game-container')
+    const ground = document.querySelector('.ground')
+
+    let duckLeft = 220
+    let duckBottom = 100
+})
